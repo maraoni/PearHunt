a6d3671f1673e2d5f30ddc9e5133b6540b133311
Marreius2005 <Marius.Sommer@edu.futuregames.se>
2026-01-13 16:10:56 +0100

    Made a sound manager and audio mixer and some folders to keep things neat

---
+++ PearHunt/Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public struct SoundInstance
+{
+    [SerializeField] private AudioSource source;
+    public SoundEffects effects;
+
+    public void PlaySoundEffect()
+    {
+        source.Play();
+    }
+}
+
+public enum SoundEffects
+{
+   
+}
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] private List<SoundInstance> soundInstances = new();
+
+    public void PlaySoundEffect(SoundEffects anEffect)
+    {
+        for (var i = 0; i < soundInstances.Count; i++)
+            if (soundInstances[i].effects == anEffect)
+            {
+                soundInstances[i].PlaySoundEffect();
+                return;
+            }
+    }
+
+    #region Singleton
+
+    public static SoundManager Instance;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Debug.LogError("[SoundManager] Mutiple soundmanagers!");
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
+
+    #endregion
+}
