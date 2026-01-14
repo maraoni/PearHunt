ae184a684d5fa55c660fc315b3f7f6be3b0f6e9c
Khaled.Zraiqi <halo_434000@hotmail.com>
2026-01-14 12:43:35 +0100

    camera collision stuff.

--- PearHunt/Assets/Scripts/CameraController.cs
+++ PearHunt/Assets/Scripts/CameraController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
@@ -18,35 +19,82 @@ public class CameraController : MonoBehaviour
     [Header("Camera Settings")] [SerializeField]
     private Camera playerCamera;
 
+    [SerializeField] private LayerMask cameraBlockingLayer;
+
     [Header("Player Camera Settings")] [SerializeField]
-    private float lookSensitivity = 10f;
+    private float lookSensitivity = 50f;
 
     [SerializeField, Range(-2, -10)] private float cameraOffset = -5f;
-    [SerializeField] private float HorizontalClamp = 80f;
+    [SerializeField, Range(50, 90)] private float HorizontalClamp = 80f;
     [SerializeField] private float targetBodyRotateSpeed = 10f;
+    [SerializeField] private float cameraResetSpeed = 2f;
 
     // Helps to determine the look direction
     private Vector2 _lookDirection = Vector3.zero;
     private Vector2 _playerTargetRotation = Vector3.zero;
+    private Vector3 _targetCenter;
+    private Coroutine _cameraPositionResetCoroutine;
 
     public void InitializeCamera(Transform aTarget)
     {
         Target = aTarget;
-        
+
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        
+        playerCamera.transform.localPosition = new Vector3(0f, 0f, cameraOffset);
+        _targetCenter = Target.gameObject.GetComponent<Collider>().bounds.center;
+        //Debug.Log(_targetCenter);
+        transform.position += new Vector3(0f, _targetCenter.y, 0f);
     }
 
     void Update()
     {
         if (Target == null) return;
+        
+        CheckIfCameraIsBlocked(); // todo: maybe needs to be checked every .25 seconds for optimization
+        UpdateCameraOnLook();
+    }
 
+    private void CheckIfCameraIsBlocked()
+    {
+        // Raycast from us to camera
+        Vector3 direction = playerCamera.transform.position - transform.position;
+        Vector3 start = Target.position + new Vector3(0f, _targetCenter.y, 0f);
+
+        if (Physics.Raycast(start, direction, out RaycastHit hit, -cameraOffset, cameraBlockingLayer))
+        {
+            // Stop it if it's active
+            if (_cameraPositionResetCoroutine != null)
+            {
+                StopCoroutine(_cameraPositionResetCoroutine);
+                _cameraPositionResetCoroutine = null;
+            }
+            
+            if (hit.collider)
+            {
+                playerCamera.transform.localPosition = new Vector3(0f, 0f,
+                    -Mathf.Max(Vector3.Distance(start, hit.point), 1f));
+            }
+        }
+        else
+        {
+            // Check if we need to reset
+            if (playerCamera.transform.localPosition.Equals(new Vector3(0f, 0f, cameraOffset)) is false)
+            {
+                _cameraPositionResetCoroutine ??= StartCoroutine(ResetCameraLocalPosition());
+            }
+        }
+
+        // todo: remove later maybe
+        Debug.DrawRay(start, direction, Color.red);
+    }
+
+    private void UpdateCameraOnLook()
+    {
         // Fetch mouse axis data
         float mouseX = Input.GetAxis("Mouse X") * lookSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * lookSensitivity * Time.deltaTime;
-
-        // Offset camera distance from body
-        playerCamera.transform.localPosition = new Vector3(0f, 0f, cameraOffset);
 
         //camera logic here
         _lookDirection.x += mouseX;
@@ -61,5 +109,18 @@ public class CameraController : MonoBehaviour
 
         // Apply the rotation to the camera
         transform.rotation = Quaternion.Euler(_lookDirection.y, _lookDirection.x, 0f);
+    }
+
+    private IEnumerator ResetCameraLocalPosition()
+    {
+        Vector3 start = playerCamera.transform.localPosition;
+        
+        for (float t = 0; t < 1f; t += Time.deltaTime * cameraResetSpeed)
+        {
+            playerCamera.transform.localPosition = Vector3.Lerp(start, new Vector3(0f, 0f, cameraOffset), t);
+            yield return null;
+        }
+
+        _cameraPositionResetCoroutine = null;
     }
 }
